44998898889e0c039b4e8c05c320e64e71a2d473
prChoe <massivcode@gmail.com>
2017-02-26 17:00:23 +0900

    전달하기 이전에 데이터베이스 계정 관련 정보 제거 및 주석 작성 완료

--- database/DatabaseConfiguration.js
+++ database/DatabaseConfiguration.js
@@ -4,11 +4,11 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-    host: 'wcwsbs.iptime.org',
-    port: 10689,
-    user: 'massivcode',
-    password: 'cpr1763',
-    database: 'joljak',
+    host: '', // <= 데이터베이스 URL 지정하세요
+    port: 0, // <= 데이터베이스 포트 숫자 지정하세요
+    user: '', // <= 데이터베이스 계정명 입력하세요
+    password: '', // <= 데이터베이스 비밀번호 입력하세요
+    database: '', // <= 데이터베이스 스키마 이름 입력하세요
     connectionLimit: 3,
     waitForConnections: true,
     supportBigNumbers: true,
