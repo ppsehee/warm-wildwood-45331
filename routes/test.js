5d22d9c3bf71a0c339f10d1dc806d22977b36c3c
sus3311 <sus3311@naver.com>
2017-03-27 17:22:10 +0900

    db test

---
+++ routes/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var pool = require('../database/DatabaseConfiguration').pool;
+var express = require('express');
+var router = express.Router();
+
+router.get('/dbtest', function (req, res) {    
+    const sql = "SELECT * FROM info WHERE receiver = ? AND caller = ? ORDER BY time DESC";
+
+    pool.getConnection.query('SELECT * FROM t_users', function (err, rows, fields) {
+        if(err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['DB test', rows]);
+    });
+});
